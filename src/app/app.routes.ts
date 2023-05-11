50ea13c04722bcdae715a1c37da108a0cd594b19
GabrielNaran <102691523+GabrielNara@users.noreply.github.com>
2023-05-11 17:34:23 -0300

    Creacion de carrito
    Genere el componente carrito y que el ruteo funcione

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./componentes/home/home.component";
 import {RegistrarComponent} from "./componentes/registrar/registrar.component";
+import {CarritoComponent} from "./componentes/carrito/carrito.component";
+
 
 
 const APP_ROUTES: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'registrar', component: RegistrarComponent },
+  { path: 'carrito', component: CarritoComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'home'},
 ];
 
