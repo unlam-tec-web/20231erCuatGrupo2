31954ec4fd2eadf98cdc36278444469c347c6455
GabrielNaran <102691523+GabrielNara@users.noreply.github.com>
2023-05-25 22:59:30 -0300

    .
    .

--- src/app/componentes/cursos/cursos.component.ts
+++ src/app/componentes/cursos/cursos.component.ts
@@ -7,34 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  categoryList: any[] = [
+  categoriaLista: any[] = [
     { id: 0, name: 'Todos los cursos' },
     { id: 1, name: 'Programación' },
     { id: 2, name: 'Diseño' },
     { id: 3, name: 'Música' }
   ];
 
-  dataList: any[] = [
+  datosLista: any[] = [
     { id: 1, title: 'Curso de Programación', price: '3000,00$', description: 'En este curso se verá cómo crear aplicaciones y sitios web desde cero con PHP y MYSQL.', image: 'php-desde-cero.jpg' },
    // { id: 2, title: 'Curso de Diseño Gráfico', price: '2000,00$', description: 'Aprende las técnicas de diseño gráfico más populares y crea diseños increíbles.', image: 'diseno-grafico.jpg' },
     { id: 1, title: 'Curso Spring', price: '30,00$', description: 'Construye aplicaciones web con Spring Framework 5 & Spring Boot: Thymeleaf, JPA, Security, REST, MySQL, Angular, WebFlux.', image: 'curso-spring.jpg' },
     { id: 3, title: 'Curso de Piano', price: '1000,00$', description: 'Aprende a tocar el piano desde cero y mejora tus habilidades musicales.', image: 'curso-piano.jpg' }
   ];
 
-  filteredList: any[] = [];
+  listaFiltrada: any[] = [];
 
   ngOnInit(): void {
-    this.filteredList = [...this.dataList];
+    this.listaFiltrada = [...this.datosLista];
   }
 
-  categoriasFiltradas(categoryId: number): void {
-    if (categoryId === 0) {
-      this.filteredList = [...this.dataList]; // Mostrar todos los cursos
+  categoriasFiltradas(categoriaId: number): void {
+    if (categoriaId === 0) {
+      this.listaFiltrada = [...this.datosLista]; // Mostrar todos los cursos
     } else {
-      this.filteredList = this.dataList.filter(item =>
-        item.id === categoryId
+      this.listaFiltrada = this.datosLista.filter(item =>
+        item.id === categoriaId
       );
     }
   }
+
 }
 
