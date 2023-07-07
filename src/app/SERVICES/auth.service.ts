7ed948b9faeb3e0326ad8b95cfc352320888051e
GabrielNaran <102691523+GabrielNara@users.noreply.github.com>
2023-07-07 15:21:03 -0300

    .
    .

--- src/app/SERVICES/auth.service.ts
+++ src/app/SERVICES/auth.service.ts
@@ -78,35 +78,6 @@ interface Response {
 
 
 
-/* Esto funciona
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { Observable } from 'rxjs';
-
-
-@Injectable({
-  providedIn: 'root'
-})
-export class AuthService {
-  private isAuthenticatedSubject: BehaviorSubject<boolean>;
-
-  constructor() {
-    this.isAuthenticatedSubject = new BehaviorSubject<boolean>(false);
-  }
-
-  public isAuthenticated(): boolean {
-    return this.isAuthenticatedSubject.value;
-  }
-
-  public setAuthenticated(authenticated: boolean): void {
-    this.isAuthenticatedSubject.next(authenticated);
-  }
-
-  public getAuthenticatedObservable(): Observable<boolean> {
-    return this.isAuthenticatedSubject.asObservable();
-  }
-}
-*/
 
 
 
